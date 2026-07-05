de89feaf41d752553f6c85193ca2c56e8f853677
sajad yoosuf <sajadyoosuf.dev@gmail.com>
2026-07-05 21:29:53 +0530

    Fix Vite PWA build cache limit

--- smart_akshaya_web_application/vite.config.js
+++ smart_akshaya_web_application/vite.config.js
@@ -37,6 +37,9 @@ export default defineConfig({
             purpose: 'any maskable'
           }
         ]
+      },
+      workbox: {
+        maximumFileSizeToCacheInBytes: 10 * 1024 * 1024 // 10 MB
       }
     })
   ],
